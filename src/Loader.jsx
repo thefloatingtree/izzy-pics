056bf73ab5b70723a1fad1bfa7dc324277b31ac7
Tree <thefloatingtree@gmail.com>
2021-10-24 21:58:48 -0600

    added izzy quotes

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,12 +1,21 @@
 import { Heading, HStack, VStack } from "@chakra-ui/layout";
 import { Spinner } from "@chakra-ui/spinner";
 
+const izzyQuotes = [
+    "A glow-up? Honey, you came to the right cottage...",
+    "Now that's what I call a glow-up!",
+    "Hi, new friend! My name's Izzy!",
+    "Do Earth ponies also like staring contests?",
+    "I will need a box of macaroni, a tube of glue, fourteen gooey bunnies, and three jelly beans. Oh. And glitter. Lots of glitter.",
+    "We're all in this together, right?"
+]
+
 export function Loader() {
     return (
         <VStack justify="center" height="100vh">
             <HStack align="center">
                 <VStack spacing="30px">
-                    <Heading size="lg" textAlign="center" color="#DD64A1">Loading Izzy from Derpibooru</Heading>
+                    <Heading size="lg" textAlign="center" color="#DD64A1">{izzyQuotes[Math.floor(Math.random() * izzyQuotes.length)]}</Heading>
                     <Spinner color="#DD64A1" thickness="3px" size="lg" />
                 </VStack>
             </HStack>
